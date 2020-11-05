5913a2370ba44bb6ba03c8ce4682147084697e40
YannickCardini <yannick.cardini@gmx.fr>
2020-11-05 11:47:45 +0100

    prise en compte du retour 503 et update readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # IAetLangue
 Conception et implémentation d’un système de questions réponses en langue naturelle sur des données structurées
+
+# Executer
+
+```bash
+python3 tp3.py
+```
+
+# Comment ca marche
+
+Les grandes étapes:
+- Récupérer les questions du fichier *question.xml*
+- Séparer chaque mots en un token
+- Tagger ces tokens
+- En extraire des d'entités nommées
+- Récupérer le [QQOQCCP](https://fr.wikipedia.org/wiki/QQOQCCP) 
+- Recupérer l'entité dans la base DBpedia à l'aide du QQOQCCP et des entités nommées
+- Récupérer le dbo ou dbp dans le fichier *relations.txt* en selectionnant celui qui a le plus de similarité avec n'importe quel token de la question
+- Requeter DBpedia avec notre entité et notre dbo/dbp
+- Analyse de la réponse 
+> :warning: Dépendant des états de service des api de dbpedia
--- tp3.py
+++ tp3.py
@@ -11,31 +11,33 @@ from pathlib import Path
 from nltk.corpus import wordnet as wn
 import json
 from collections import Counter
-
+import sys
 
 url = Path(os.getcwd())
+
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
-def getBestWords(nodes,intWord):
+
+def getBestWords(nodes, intWord):
     res = None
     if len([x for x in nodes if x[0] is None]) == len(nodes):
-        return( "_".join([i[1] for i in nodes]))
+        return("_".join([i[1] for i in nodes]))
     else:
         if intWord == "who" or intWord == "Who":
-            res = [y for x,y in nodes if x == "PERSON" ]
+            res = [y for x, y in nodes if x == "PERSON"]
             if len(res) == 0:
-                return nodes[0][1]
+                return nodes[0]
             return res[0]
         elif intWord == "when" or intWord == "When":
-            res = [y for x,y in nodes if x == "DATE" ]
+            res = [y for x, y in nodes if x == "DATE"]
             if len(res) == 0:
-                return nodes[0][1]
+                return nodes[0]
             return res[0]
 
 
-def getNodes(parent,intWord):
+def getNodes(parent, intWord):
     lbwd = []
     for node in parent:
         if type(node) is nltk.Tree:
@@ -44,72 +46,83 @@ def getNodes(parent,intWord):
                 words = ""
                 for word in node.leaves():
                     words += word[0] + " "
-                lbwd.append((label,words))
-            getNodes(node,intWord)
+                lbwd.append((label, words))
+            getNodes(node, intWord)
         else:
             if(node[1] == "NNP" or node[1] == "NNPS"):
-                lbwd.append((None,node[0]))
+                lbwd.append((None, node[0]))
     if len(lbwd) > 1:
-        return getBestWords(lbwd,intWord)
+        return getBestWords(lbwd, intWord)
     elif len(lbwd) == 1:
         return lbwd[0][1]
     else:
         return None
 
+
 def getInterrogativeWord(phrase):
     if(type(phrase) == list):
         phrase = " ".join(phrase)
-    match = re.search('(([wW])(here|ho|hat|hich|hen))|([hH]ow)',phrase)
+    match = re.search('(([wW])(here|ho|hat|hich|hen))|([hH]ow)', phrase)
     if (match != None):
         return match.group()
     else:
         return None
 
+
 def query(q, f='application/json'):
     try:
         params = {'query': q}
-        resp = requests.get("http://dbpedia.org/sparql", params=params, headers={'Accept': f})
+        resp = requests.get("http://dbpedia.org/sparql",
+                            params=params, headers={'Accept': f})
         return resp.text
     except Exception as e:
         print(e)
         raise
 
+
 def getKeyword(queryString):
     if(queryString == None):
         return ""
-    resp = requests.get("https://lookup.dbpedia.org/api/search/PrefixSearch?&MaxHits=1&QueryString=" + queryString, headers={'Accept': 'application/json'})
+    query = "https://lookup.dbpedia.org/api/search/PrefixSearch?&MaxHits=1&QueryString=" + queryString
+    resp = requests.get(query, headers={'Accept': 'application/json'})
+    if (resp.status_code == 503):
+        print('\033[91m Service Unavailable, dbpedia is down, unable to reach ' + query + ' please try again later !\033[0m')
+        sys.exit()
     tree = ET.fromstring(resp.content)
     for child in tree:
         for element in child:
             if(element.tag == "URI"):
                 return element.text.split("/")[-1]
     return ""
-    
+
+
 def getRelations():
     dbo = []
     dbp = []
-    fs = open(url / "relations.txt",'r')  
+    fs = open(url / "relations.txt", 'r')
     for line in fs.readlines():
         db, relation = line.split(":")
         if(db == "dbo"):
             dbo.append(re.sub("\n", "", relation))
         else:
             dbp.append(re.sub("\n", "", relation))
-    return (dbo,dbp)
+    return (dbo, dbp)
+
 
 def getBestSimilarity(token):
     if(token == None):
         return None
     dbo, dbp = getRelations()
     for db in dbo:
-        similarity = similar(token,db)
-        if( similarity > 0.7):
-            return db,similarity,"dbo"
+        similarity = similar(token, db)
+        if(similarity > 0.7):
+            return db, similarity, "dbo"
     for db in dbp:
-        similarity = similar(token,db)
-        if( similarity > 0.7):
-            return db,similarity,"dbp"
+        similarity = similar(token, db)
+        if(similarity > 0.7):
+            return db, similarity, "dbp"
     return None
+
 
 def getPathSimilarity(token):
     if(token == None):
@@ -117,23 +130,23 @@ def getPathSimilarity(token):
     tokenSyn = wn.synsets(token)
     if(len(tokenSyn) == 0):
         return None
-    similarity = [] 
+    similarity = []
     dbo, dbp = getRelations()
     for db in dbo:
         dbSyn = wn.synsets(db)
         if(len(dbSyn) > 0):
-            simTuple = (db,tokenSyn[0].path_similarity(dbSyn[0]),"dbo")
+            simTuple = (db, tokenSyn[0].path_similarity(dbSyn[0]), "dbo")
             if(simTuple[1] != None):
                 similarity.append(simTuple)
     for db in dbp:
         dbSyn = wn.synsets(db)
         if(len(dbSyn) > 0):
-            simTuple = (db,tokenSyn[0].path_similarity(dbSyn[0]),"dbp")
+            simTuple = (db, tokenSyn[0].path_similarity(dbSyn[0]), "dbp")
             if(simTuple[1] != None):
                 similarity.append(simTuple)
     if(len(similarity) == 0):
         return None
-    dbo = max(similarity,key=lambda item:item[1])
+    dbo = max(similarity, key=lambda item: item[1])
     return dbo
 
 
@@ -142,28 +155,30 @@ def getdb(tokens):
     dbs = []
     for token in tokens:
         dbs.append(getBestSimilarity(token))
-    dbs = [i for i in dbs if i]#Remove None
+    dbs = [i for i in dbs if i]  # Remove None
     if len(dbs) == 0:
         for token in tokens:
             dbs.append(getPathSimilarity(token))
-        dbs = [i for i in dbs if i]#Remove None
+        dbs = [i for i in dbs if i]  # Remove None
     if len(dbs) > 1:
-        db = max(dbs,key=lambda item:item[1])
+        db = max(dbs, key=lambda item: item[1])
     else:
-        db = dbs[0]       
+        db = dbs[0]
     return db
+
 
 def getResp(q1):
     res = []
     try:
         json_dictionary = json.loads(q1)
     except:
-        print('\033[91m',"Echec de la requête:\n",q1,'\033[0m')
+        print('\033[91m', "Echec de la requête:\n", q1, '\033[0m')
         return []
     bindings = json_dictionary["results"]["bindings"]
     for uri in bindings:
         res.append(uri["uri"]["value"])
     return res
+
 
 questionXML = url / "questions.xml"
 tree = ET.parse(os.path.join(questionXML))
@@ -183,23 +198,25 @@ for child in root:
             tagged = nltk.pos_tag(tokens)
             entities = nltk.chunk.ne_chunk(tagged)
             intWord = getInterrogativeWord(question.text)
-            word = getNodes(entities,intWord)
+            word = getNodes(entities, intWord)
             db = getdb(tokens)
-            db_request = 'PREFIX res: <http://dbpedia.org/resource/>PREFIX dbp: <http://dbpedia.org/property/>' if db[2] == "dbp" else 'PREFIX res: <http://dbpedia.org/resource/>PREFIX dbo: <http://dbpedia.org/ontology/>'
-            select_request = 'SELECT DISTINCT ?uri WHERE { res:' + getKeyword(word) + " " + db[2] + ":" + db[0] + ' ?uri .}'
-            q1 = db_request + select_request   
+            db_request = 'PREFIX res: <http://dbpedia.org/resource/>PREFIX dbp: <http://dbpedia.org/property/>' if db[
+                2] == "dbp" else 'PREFIX res: <http://dbpedia.org/resource/>PREFIX dbo: <http://dbpedia.org/ontology/>'
+            select_request = 'SELECT DISTINCT ?uri WHERE { res:' + getKeyword(
+                word) + " " + db[2] + ":" + db[0] + ' ?uri .}'
+            q1 = db_request + select_request
             resp = getResp(query(q1))
         if(question.tag == 'answers'):
             for answer in question:
                 for uri in answer:
                     gold_standard_answers.append(uri.text)
     print(questions[-1])
-    print(db[2],db[0])
-    print("Word(s): ", getKeyword(word))
-    print("Reponse: ",resp)
+    print(db[2], db[0])
+    print("Word(s): ", word, getKeyword(word))
+    print("Reponse: ", resp)
     c = Counter(resp)
     nb_bonne_reponse = sum(c[x] for x in gold_standard_answers)
-    print("Nombre de bonnes réponses: ",nb_bonne_reponse,"\n")     
+    print("Nombre de bonnes réponses: ", nb_bonne_reponse, "\n")
     total_bonne_rep += nb_bonne_reponse
     total_gold_rep += len(gold_standard_answers)
     total_rep += len(resp)
@@ -207,5 +224,5 @@ for child in root:
 recall = total_bonne_rep/total_gold_rep
 prescion = total_bonne_rep/total_rep
 print("Recall: ", recall)
-print("Précision: ",prescion)
+print("Précision: ", prescion)
 print("F-measure: ", (2*prescion*recall)/(prescion+recall))
